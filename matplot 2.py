84e070d2929f54347e7db1f418ac38867fba42d7
Abdulrahman <43207424+ABDULRAHMANADAWY@users.noreply.github.com>
2019-08-24 03:56:04 +0200

    Update matplot 2.py

--- matplot 2.py
+++ matplot 2.py
@@ -7,14 +7,20 @@ Created on Fri Aug 16 01:01:29 2019
  import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-
+# Histogram
 Bins = [40,50,60,70,80,90,100]
+# resize our Graph
 plt.figure(figsize=(8,5))
+# plot our figure in histogram and determine color  
 plt.hist(fifa.Overall, Bins=Bins, color='g')
+# X axis Tickmarks (scale of our graph)
 plt.xticks(Bins)
+# X , Y Labels
 plt.ylabel('Number of Players')
 plt.xlabel('Skill Level')
+# write a title 
 plt.title('Distribution of Player Skills in FIFA 2018')
+# save figure
 plt.savefig('histogram.png', dpi=300)
 plt.show()
   
